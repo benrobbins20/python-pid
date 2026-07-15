a516711563354032d24a0255c259f4b823bf5580
Benjamin Robbins <benro2211@gmail.com>
2026-07-14 21:41:00 -0600

    P controller with staged velo, not working

--- assignment1.py
+++ assignment1.py
@@ -14,15 +14,14 @@ options['PEOPLE_MASS'] = 0
 # Controller Options
 options['CONTROLLER'] = True
 options['START_LOC'] = 0
-options['SET_POINT'] = 20 # transition point
+options['SET_POINT'] = 15 # transition point
 options['OUTPUT_GAIN'] = 1000 # elevator plus counterweight to start
 
 class Controller:
     def __init__(self, reference):
         self.r = reference # fixed halfway reference point
         self.prev_time = 0
-        self.prev_pos = 0
-        self.prev_velo = 0
+        self.prev_x = 0
         self.output = 0
     def run(self, x, t):
         # block to control only 20hz
@@ -30,57 +29,46 @@ class Controller:
         if t - self.prev_time < 0.05:
             return self.output
         else:
+            if (abs(x-self.r) < 0.5):
+                print(f"hit setpoint {self.r}:{x}:{t}")
+                
             # distance away, +error is under set point, -error is over set point 
-            position_error = self.r - x
-            direction = 1 if position_error > 0 else -1
-            dist = abs(position_error)
+            error = self.r - x
+            direction = 1 if error > 0 else -1
+            self.prev_time = t
+            self.prev_x = x
             
             # speed gain
             fast = 4
             slow = 1
             # relative distance from setpoint
-            far = 10
-            close = 5
-            stop = 1
+            far = 20
+            close = 10
+            stop = 2
             
-            # linear slow down in the slowing range
+            
             slope = (fast - slow) / (far - close)
             
-            # 
-            if dist > far:
-                desired_velo = fast * direction
-            elif dist > close:
-            
-                target_speed = (dist - close) * slope + slow
-                desired_velo = (target_speed * direction)
-            elif dist > stop:
-                desired_velo = slow * direction
-            elif dist > 0.01:
-                desired_velo = 0.1 * direction
+            # furthest away, full output
+            if abs(error) > far:
+                self.output = fast * direction
+            elif abs(error) > close:
+                # linear acceleration from 19-10
+                _y = (abs(error) - close) * slope + slow
+                self.output = (_y * direction)
+            elif abs(error) > stop:
+                self.output = slow * direction
             else:
-                desired_velo = 0
+                # no power, just let gravity move elevator
+                self.output = 0
+            
+            
+            # if x < self.r:
+            #     self.output = 10
+            # else:
+            #     self.output = -10
                 
-            # dt = 0.05?
-            DT = 0.05
-            actual_velo = (x - self.prev_pos) / (DT)
-            
-            velocity_error = desired_velo - actual_velo
-            self.output = velocity_error
-            
-            accel = (actual_velo - self.prev_velo) / DT
-            
-            self.prev_time = t
-            self.prev_pos = x
-            self.prev_velo = actual_velo
-            
-            # print(f"t:{t:.2f} x:{x:.2f} v:{actual_velo:.2f} a:{accel:.2f}")
-            print(
-                f"x={x:.2f} desired_v={desired_velo:.2f} "
-                f"actual_v={actual_velo:.2f} "
-                f"v_err={velocity_error:.2f} "
-                f"return_desired={desired_velo:.2f} "
-                f"return_error={velocity_error:.2f}"
-            )
-            # return self.output
-            return desired_velo
+            # self.output = 4
+            # INSERT CODE ABOVE.
+            return self.output
 sim_run(options, Controller)
