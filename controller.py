de8404829dcc079f1c9f4504bc7e2be7d4d6b186
Benjamin Robbins <benro2211@gmail.com>
2026-07-26 14:36:14 -0600

    comments on sign change reset, very stable down->up and up->down

--- controller.py
+++ controller.py
@@ -17,14 +17,14 @@ options['COUNTERWEIGHT_MASS'] = 1000
 options['PEOPLE_MASS'] = 200
 # Controller Options
 options['CONTROLLER'] = True
-options['START_LOC'] = 27
-options['SET_POINT'] = 3
+options['START_LOC'] = 3
+options['SET_POINT'] = 27
 options['OUTPUT_GAIN'] = 2000
 options['MAX_ACCEL'] = 5
 
 class Controller:
     Kp = 5
-    Ki = 0.08
+    Ki = 0.25
     Kd = 8
     g = 9.8
     
@@ -42,21 +42,36 @@ class Controller:
         self.gravity_hold = (self.payload_mass * (self.g)) / options['OUTPUT_GAIN'] # normalize output to controller output range
         # clip the gravity hold output to max output of the system
         # this is when all other PID outputs are zero and system needs to hold payload in place
-        self.hold_ratio = np.clip(self.gravity_hold / self.max_output, -1, 1)
+        self.hold_ratio = np.clip(self.gravity_hold / self.max_output, -0.999, 0.999) # strictly feed arctan < +-1
         # # inverse tanh for ff, reduces math error for tanh saturationwhen arctanh processed holding output
         self.g_feed_forward = self.max_output * np.arctanh(self.hold_ratio) # arctanh(0.196) = 0.19856, output = 0.19856 * 5 = 0.9928
 
-        self.start_error = options['SET_POINT'] - options['START_LOC']
+        # provides initial error state and sign, used for integral reset
+        self.previous_error = options['SET_POINT'] - options['START_LOC']
         
     def run(self, t, x, v, dt=None):
         position_error = self.r - x
         
         # after solver.integrate(), run(is called with dt, which is the signal to update the state)
         if dt is not None:
-            self.integral += position_error * dt
+            
+            # integral reset if sign changes (overshoot/undershoot)
+            # regular state: +pos_error * +prev_error = + no sign change
+            # up->down -pos_error * +prev_error = - sign change, below the target
+            # down->up + +pos_error * -prev_error = - sign change, above the target
+            if position_error * self.previous_error < 0:
+                self.integral = 0
+                
+            # only accumulate steady state error < 1m
+            if abs(position_error) < 1:
+                self.integral += position_error * dt
+                
             # hard limit to windup bounds
             self.integral = np.clip(self.integral, -self.max_windup, self.max_windup)
-    
+
+            # update previous error inside accepted timestep condition
+            self.previous_error = position_error
+            
         self.p_out = self.Kp * position_error
         self.i_out = self.Ki * self.integral
         self.d_out = -self.Kd * v
